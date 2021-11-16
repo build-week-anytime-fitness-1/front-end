f9335685fae5129b5f8dd5d6a4de9e6ced1adaf2
Gaston Irby III <gaston@gastons-imac.lan>
2021-11-16 10:50:21 -0800

    revamped the create a class

--- anytime-fitness/src/App.js
+++ anytime-fitness/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import InstructorForm from './components/InstructorForm';
 import UserForm from './components/UserForm'; 
+import ClassCreate from './components/classCreate';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <UserForm />
-        <InstructorForm />
+        <ClassCreate />
+        {/* <UserForm />
+        <InstructorForm /> */}
       </header>
     </div>
   );
