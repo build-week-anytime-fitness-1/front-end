4166c03199c935345fea7605b4d7258a5bb3c211
Richard Perry <85124966+rdperry031@users.noreply.github.com>
2021-11-16 18:36:29 -0600

    cloned front-end and some routing, fixed local duplicate folder issue (#12)
    * cloned front-end and some routing
    
    * hopefully fixed duplicate folder issue
    
    * ...
    
    * ...
    
    * still attempting to correct the duplicate folder issue.
    
    * duplicate folder fix attempt
    
    * Update index.js

--- anytime-fitness/src/App.js
+++ anytime-fitness/src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
-
 import InstructorForm from './components/InstructorForm';
 import UserForm from './components/UserForm'; 
 import ClassCreate from './components/ClassCreate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ClassCreate />
-        {/* <UserForm />
-        <InstructorForm /> */}
-      </header>
+    <div className='App'>
+      <header className='App-header'>
+        {<InstructorForm/>}
+          </header>
     </div>
   );
 }
